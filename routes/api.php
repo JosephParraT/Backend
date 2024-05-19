e9467c9835b9ee7f3e12dd55693d5891b9076bf9
joseph parra <josephpt2004@outlook.com>
2024-05-19 16:35:21 -0500

    Creacion de rutas

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\API\HabitacionController;
+use App\Http\Controllers\API\ReservaController;
+use App\Http\Controllers\API\ClienteController;
+use App\Http\Controllers\API\ServicioAdicionalController;
+use App\Http\Controllers\API\PagoController;
+
+Route::apiResource('habitaciones', HabitacionController::class);
+Route::apiResource('reservas', ReservaController::class);
+Route::apiResource('clientes', ClienteController::class);
+Route::apiResource('servicios-adicionales', ServicioAdicionalController::class);
+Route::apiResource('pagos', PagoController::class);
